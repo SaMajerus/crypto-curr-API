a9b422ca68c55bd6b67ea18c0aa57e7ccf4e8891
Harold_Mesa <harold.mesa@icloud.com>
2022-09-07 18:55:23 -0500

    WIP: Working on exchange()

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-// Business Logic
+// Business Logic for crypto
 function crypto(currency) {
   let promise = new Promise(function(resolve, reject){
     let request = new XMLHttpRequest();
@@ -10,7 +10,7 @@ function crypto(currency) {
     request.addEventListener("loadend", function(){
       const response = JSON.parse(this.responseText);
       if (this.status === 200) {
-        resolve([response, currency,]);
+        resolve([response, currency]);
       } else {
         reject([this, response, currency]);
       }
@@ -26,14 +26,50 @@ function crypto(currency) {
   });
 }
 
-// UI Logic 
+// Business Logic for exchange
+/* function exchange(from, to) {   //Calculates the current price of a given cryptocurrency in another currency like USD, Euro, etc. 
+  let promise = new Promise(function(resolve, reject){
+    let request = new XMLHttpRequest();
+    const url = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
+    request.addEventListener("loadend", function(){
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve([response, from, to]);
+      } else {
+        reject([this, response, from, to]);
+      }
+    });
+    request.open("GET",url,true);
+    request.send();
+  });
+
+  promise.then(function(responseDataArray){
+    printElements1(responseDataArray);
+  }, function(errorMessage) {
+    printError1(errorMessage);
+  });
+}
+
+
+// UI Logic (for 'exchange' Business Logic only)
+function printElements1(data) {
+  document.querySelector().innerText = 
+  `from ${data[0].from} to ${data[0].to}:  ${data[0]}`;  
+}
+
+function printError1(errorMsg) {
+  document.getElementById('response').innerText = `You have the following Error:  ${errorMsg}`;
+}  */
+
+
+// UI Logic (for 'crypto' Business Logic only)
 function printElements(data) {
-  document.querySelector('div#response').innerText = 
-  `Name: ${data[0][0].name}
-  Price:  ${Math.round(data[0][0].price *100) / 100}    
+  document.getElementById("responseContent").innerText = `
+  Name: ${data[0][0].name}
+  Price: $${Math.round(data[0][0].price *100) / 100}    
   Rank: ${data[0][0].rank}
 
-  
+
   Currency Status:  ${data[0][0].status}
   Number of currently-occurring trades for this currency:  ${data[0][0].num_exchanges}
 
@@ -49,9 +85,10 @@ function printError(errorMsg) {
 
 function handleFormSubmission(event) {
   event.preventDefault();
+  document.getElementById("response").removeAttribute("class");
   const currency = document.querySelector('input#uIn1').value;
-  document.querySelector('input#uIn1').value = null;
   crypto(currency); 
+  //exchange();
 }
 
 window.addEventListener("load", function() {
